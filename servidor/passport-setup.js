0325e8edd9d960331d722573395b89d9e3dc6408
sergio <sergio.garcia-romero@stud.uni-bamberg.de>
2023-01-14 19:05:29 +0100

    Eliminado el .env

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -10,9 +10,9 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: env.ID_CLIENTE,
-    clientSecret: env.SECRET,
-    callbackURL: env.URL_CALLBACK + "/google/callback"
+    clientID: "455903968700-bo2cug7jke03u8qn66poe1453v4ip8sd.apps.googleusercontent.com",
+    clientSecret: "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",
+    callbackURL: "https://procesos2-ij2yuy7u7a-ew.a.run.app/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     return done(null, profile);
